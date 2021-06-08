444f48bca5c987d8d7162a8c8eb2dd73e10636b1
William Ferebee <williamf@tahzoo.com>
2021-06-08 11:15:10 -0400

    added comments while going through Preethis code for the first time. I should probably go back and add them to mine as well.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -6,7 +8,74 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+
+
+      
+
             Console.WriteLine("Hello World!");
+
+            List<string> myList = Toppings(toppings, 5);
+            Console.WriteLine("\n");
+
+            LogInstructions(myList);
+
+            Console.ReadLine();
         }
+
+        static string[] toppings = { "Lettuce", "Onion", "Steak", "Cheese" }; //Simulation of what comes on a taco
+
+        public static List<string> Toppings(string[] toppings, int tacoTime) {
+
+
+            List<string> instructions = new List<string>(); //Log Process of taco Send to William
+            List<int> instructionsTime = new List<int>(); //Time it takes -> Send to William  
+
+            //int tacoTime = 0; //How long does the taco take -> Send to Shiza
+
+            if (toppings.Contains("Lettuce")) {
+              instructions.Add("Chopping lettuce");
+              instructionsTime.Add(10);
+              //instructions.ToList().ForEach(Console.WriteLine);
+              tacoTime += 10;
+            }
+            if (toppings.Contains("Onion")) {
+              instructions.Add("Slicing Onion");
+              instructionsTime.Add(10);
+              //instructions.ToList().ForEach(Console.WriteLine);
+              tacoTime += 10;
+            }
+            if (toppings.Contains("Steak")) {
+              instructions.Add("Grilling Steak");
+              instructionsTime.Add(30);
+              //instructions.ToList().ForEach(Console.WriteLine);
+              tacoTime += 30;
+            }
+            if (toppings.Contains("Cheese")) {
+              instructions.Add("Shredding Cheese");
+              instructionsTime.Add(5);
+              //instructions.ToList().ForEach(Console.WriteLine);
+              tacoTime += 5;
+            }
+                    //Console.WriteLine(tacoTime);Test Statement
+                    //instructions.ToList().ForEach(Console.WriteLine); Test Statement 
+                    //instructionsTime.ToList().ForEach(Console.WriteLine); Test Statement
+                    Console.WriteLine(tacoTime);
+            return instructions;
+    }
+
+        
+        public static void LogInstructions(List<string> instructionList)
+        {
+            foreach (var item in instructionList)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("\n");
+
+               
+            }
+        }
+
+
     }
 }
